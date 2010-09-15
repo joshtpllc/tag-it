00bfeabc0a6dde74c6c8f6ad50f78601b3e9689d
joshtpllc <josh@telephonypartners.com>
2010-09-15 13:50:52 -0700

    Several options added: * hiddenFieldName + hiddenFieldIndex - allows you to specify how the hidden fields are created.  This was done to work with the ASP.NET MVC model builder. * validationRegex - if present, checks entry against regex and styles the item based on an "invalid" class.
    In addition, tab keypresses are captured and used to create a new item if the input field has a value or to tab to the next control otherwise.

--- js/tag-it.js
+++ js/tag-it.js
@@ -1,100 +1,131 @@
-(function($) {
+(function ($) {
 
-	$.fn.tagit = function(options) {
+    $.fn.tagit = function (options) {
 
-		var el = this;
+        var el = this;
 
-		const BACKSPACE		= 8;
-		const ENTER			= 13;
-		const SPACE			= 32;
-		const COMMA			= 44;
+        var BACKSPACE = 8;
+        var ENTER = 13;
+        var TAB = 9;
+        var SPACE = 32;
+        var COMMA = 44;
 
-		// add the tagit CSS class.
-		el.addClass("tagit");
+        var cleanAndAdd = function () {
+            var typed = tag_input.val();
+            typed = typed.replace(/,+$/, "");
+            typed = typed.trim();
 
-		// create the input field.
-		var html_input_field = "<li class=\"tagit-new\"><input class=\"tagit-input\" type=\"text\" /></li>\n";
-		el.html (html_input_field);
+            if (typed != "" && !options.mustMatch) {
+                if (is_new(typed)) {
+                    create_choice(typed, "");
+                }
+                // Cleaning the input.
+                tag_input.val("");
+            }
+        };
 
-		tag_input		= el.children(".tagit-new").children(".tagit-input");
+        // add the tagit CSS class.
+        el.addClass("tagit");
 
-		$(this).click(function(e){
-			if (e.target.tagName == 'A') {
-				// Removes a tag when the little 'x' is clicked.
-				// Event is binded to the UL, otherwise a new tag (LI > A) wouldn't have this event attached to it.
-				$(e.target).parent().remove();
-			}
-			else {
-				// Sets the focus() to the input field, if the user clicks anywhere inside the UL.
-				// This is needed because the input field needs to be of a small size.
-				tag_input.focus();
-			}
-		});
+        // create the input field.
+        var html_input_field = "<li class=\"tagit-new\"><input class=\"tagit-input\" type=\"text\" /></li>\n";
+        el.html(html_input_field);
 
-		tag_input.keypress(function(event){
-			if (event.which == BACKSPACE) {
-				if (tag_input.val() == "") {
-					// When backspace is pressed, the last tag is deleted.
-					$(el).children(".tagit-choice:last").remove();
-				}
-			}
-			// Comma/Space/Enter are all valid delimiters for new tags.
-			else if (event.which == COMMA || event.which == SPACE || event.which == ENTER) {
-				event.preventDefault();
+        tag_input = el.children(".tagit-new").children(".tagit-input");
 
-				var typed = tag_input.val();
-				typed = typed.replace(/,+$/,"");
-				typed = typed.trim();
+        // read initial values
+        if (options.values) {
+            for (var i = 0; i < options.values.length; i++) {
+                create_choice(options.values[i].value, options.values[i].id);
+            }
+        }
 
-				if (typed != "") {
-					if (is_new (typed)) {
-						create_choice (typed);
-					}
-					// Cleaning the input.
-					tag_input.val("");
-				}
-			}
-		});
+        $(this).click(function (e) {
+            if (e.target.tagName == 'A') {
+                // Removes a tag when the little 'x' is clicked.
+                // Event is binded to the UL, otherwise a new tag (LI > A) wouldn't have this event attached to it.
+                $(e.target).parent().remove();
+            }
+            else {
+                // Sets the focus() to the input field, if the user clicks anywhere inside the UL.
+                // This is needed because the input field needs to be of a small size.
+                tag_input.focus();
+            }
+        });
 
-		tag_input.autocomplete({
-			source: options.availableTags, 
-			select: function(event,ui){
-				if (is_new (ui.item.value)) {
-					create_choice (ui.item.value);
-				}
-				// Cleaning the input.
-				tag_input.val("");
+        tag_input.keypress(function (event) {
+            if (event.which == BACKSPACE) {
+                if (tag_input.val() == "") {
+                    // When backspace is pressed, the last tag is deleted.
+                    $(el).children(".tagit-choice:last").remove();
+                }
+            }
+            // Comma/Space/Enter are all valid delimiters for new tags.
+            else if (event.which == COMMA || event.which == SPACE || event.which == ENTER || event.which == TAB) {
+                event.preventDefault();
+                cleanAndAdd();
+            }
+        });
 
-				// Preventing the tag input to be update with the chosen value.
-				return false;
-			}
-		});
+        tag_input.keydown(function (event) {
+            if (event.keyCode == TAB && tag_input.val() != "") {
+                event.preventDefault();
+                cleanAndAdd();
+            }
+        });
 
-		function is_new (value){
-			var is_new = true;
-			this.tag_input.parents("ul").children(".tagit-choice").each(function(i){
-				n = $(this).children("input").val();
-				if (value == n) {
-					is_new = false;
-				}
-			})
-			return is_new;
-		}
-		function create_choice (value){
-			var el = "";
-			el  = "<li class=\"tagit-choice\">\n";
-			el += value + "\n";
-			el += "<a class=\"close\">x</a>\n";
-			el += "<input type=\"hidden\" style=\"display:none;\" value=\""+value+"\" name=\"item[tags][]\">\n";
-			el += "</li>\n";
-			var li_search_tags = this.tag_input.parent();
-			$(el).insertBefore (li_search_tags);
-			this.tag_input.val("");
-		}
-	};
+        if (options.availableTags && options.availableTags.length > 0) {
+            tag_input.autocomplete({
+                source: options.availableTags,
+                select: function (event, ui) {
+                    if (is_new(ui.item.value)) {
+                        create_choice(ui.item.value, ui.item.Id);
+                    }
+                    // Cleaning the input.
+                    tag_input.val("");
 
-	String.prototype.trim = function() {
-		return this.replace(/^\s+|\s+$/g,"");
-	};
+                    // Preventing the tag input to be update with the chosen value.
+                    return false;
+                }
+            });
+        }
+
+        function is_new(value) {
+            var is_new = true;
+            this.tag_input.parents("ul").children(".tagit-choice").each(function (i) {
+                n = $(this).children("input").val();
+                if (value == n) {
+                    is_new = false;
+                }
+            })
+            return is_new;
+        }
+        function create_choice(value, id) {
+            var el = "";
+            var valid = true;
+            if (options.validationRegex) {
+                var re = new RegExp(options.validationRegex);
+                valid = value.match(re);
+            }
+            el = "<li class=\"tagit-choice";
+            if (!valid)
+                el += " invalid";
+            el += "\">\n";
+            el += value + "\n";
+            el += "<a class=\"close\">x</a>\n";
+            el += "<input type=\"hidden\" style=\"display:none;\" value=\"" + id + "\" name=\"" + options.hiddenFieldName;
+            if (options.hiddenFieldIndex && options.hiddenFieldIndex.length > 0)
+                el += "[" + options.hiddenFieldIndex + "]";
+            el += "\">\n";
+            el += "</li>\n";
+            var li_search_tags = this.tag_input.parent();
+            $(el).insertBefore(li_search_tags);
+            this.tag_input.val("");
+        }
+    };
+
+    String.prototype.trim = function () {
+        return this.replace(/^\s+|\s+$/g, "");
+    };
 
 })(jQuery);
